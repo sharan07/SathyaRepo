f460c72ca760aaf40006dc58ca7804c51ec684df
admin <admin@admin-PC>
2017-07-23 01:34:50 +0530

    modified by collabrator2

--- src/main/java/pack1/App.java
+++ src/main/java/pack1/App.java
@@ -9,6 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+      //change made by collabrator 1
         System.out.println( "Hello World!1" );
+        //change made by collabrator 2
+        System.out.println( "Hello World!2" );
     }
 }
